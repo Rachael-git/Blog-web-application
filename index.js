cab3e83026cbe022828bcd57742e9716ae094363
Rachael-git <r.traimittaphap@gmail.com>
2025-09-20 16:39:17 +1000

    add styles

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
 	res.render('index', { title: 'B;og Web', data })
